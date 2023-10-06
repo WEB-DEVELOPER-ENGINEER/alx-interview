aab1d37a5799ed0efbf53914b66ed5a65bb81f5d
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-06 14:18:01 -0700

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''a method that determines if all the boxes can be opened
-'''
+
+from collections import deque
 
 
 def canUnlockAll(boxes):
@@ -11,34 +11,15 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, otherwise, False
     """
-    arr = []
-    for i in range(len(boxes)):
-        arr.append(i)
-    arr.pop(0)
-    val = boxes[0]
-    return access(boxes, val, arr)
-
-
-def access(boxes, val, arr):
-    """A helper function that recursively unlocks boxes.
-    It operates on a single box at a time,
-    checking its keys to unlock other boxes.
-    Args:
-        boxes (list of lists): List of lists containing all
-        the boxes (lists) with the keys.
-        val (list): The box (list) that we are currently checking its keys.
-        arr (list): A list containing the numbers of boxes.
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False
-    """
-    if len(arr) == 0:
-        return True
-    if len(val) != 0:
-        for x in val:
-            if x in arr:
-                arr.remove(x)
-                value = boxes[x]
-                access(boxes, value, arr)
-    if len(arr) == 0:
-        return True
-    return False
+    queue = deque()
+    queue.append(0)
+    visited = set()
+    visited.add(0)
+    while queue:
+        box_idx = queue.popleft()
+        keys = boxes[box_idx]
+        for key in keys:
+            if key not in visited:
+                queue.append(key)
+                visited.add(key)
+    return len(visited) == len(boxes)
