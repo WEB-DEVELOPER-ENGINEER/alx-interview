b0f40bee9a8d6a5bab79c46d356c9cea130c8aef
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-06 14:31:35 -0700

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+'''a method that determines if all the boxes can be opened
+'''
+
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened
     Args:
@@ -6,12 +11,34 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, otherwise, False
     """
-    n = len(boxes)
-    visited = [False] * n
-    def dfs(box):
-        visited[box] = True
-        for key in boxes[box]:
-            if not visited[key]:
-                dfs(key)
-    dfs(0)
-    return all(visited)
+    arr = []
+    for i in range(len(boxes)):
+        arr.append(i)
+    arr.pop(0)
+    val = boxes[0]
+    return access(boxes, val, arr)
+
+
+def access(boxes, val, arr):
+    """A helper function that recursively unlocks boxes.
+    It operates on a single box at a time,
+    checking its keys to unlock other boxes.
+    Args:
+        boxes (list of lists): List of lists containing all
+        the boxes (lists) with the keys.
+        val (list): The box (list) that we are currently checking its keys.
+        arr (list): A list containing the numbers of boxes.
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
+    if len(arr) == 0:
+        return True
+    if len(val) != 0:
+        for x in val:
+            if x in arr:
+                arr.remove(x)
+                value = boxes[x]
+                access(boxes, value, arr)
+    if len(arr) == 0:
+        return True
+    return False
