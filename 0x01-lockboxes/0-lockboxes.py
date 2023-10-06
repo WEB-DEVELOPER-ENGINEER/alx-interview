f237650de911852e76f85f78d189db27f08eaa00
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-06 13:47:54 -0700

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,6 +4,13 @@
 
 
 def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened
+    Args:
+        boxes (list of lists): List of lists containing all
+        the boxes (lists) with the keys.
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
     arr = []
     for i in range(len(boxes)):
         arr.append(i)
@@ -13,6 +20,17 @@ def canUnlockAll(boxes):
 
 
 def access(boxes, val, arr):
+    """A helper function that recursively unlocks boxes.
+    It operates on a single box at a time,
+    checking its keys to unlock other boxes.
+    Args:
+        boxes (list of lists): List of lists containing all
+        the boxes (lists) with the keys.
+        val (list): The box (list) that we are currently checking its keys.
+        arr (list): A list containing the numbers of boxes.
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
     if len(arr) == 0:
         return True
     if len(val) != 0:
