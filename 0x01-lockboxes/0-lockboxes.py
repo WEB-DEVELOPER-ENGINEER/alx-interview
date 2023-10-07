da138f8153697fada715b2044570dbbeec65d987
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-06 17:12:10 -0700

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,34 +11,14 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, otherwise, False
     """
-    arr = []
-    for i in range(len(boxes)):
-        arr.append(i)
-    arr.pop(0)
-    val = boxes[0]
-    return access(boxes, val, arr)
-
-
-def access(boxes, val, arr):
-    """A helper function that recursively unlocks boxes.
-    It operates on a single box at a time,
-    checking its keys to unlock other boxes.
-    Args:
-        boxes (list of lists): List of lists containing all
-        the boxes (lists) with the keys.
-        val (list): The box (list) that we are currently checking its keys.
-        arr (list): A list containing the numbers of boxes.
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False
-    """
-    if len(arr) == 0:
-        return True
-    if len(val) != 0:
-        for x in val:
-            if x in arr:
-                arr.remove(x)
-                value = boxes[x]
-                access(boxes, value, arr)
-    if len(arr) == 0:
-        return True
-    return False
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+    visited[0] = True
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if not visited[key]:
+                visited[key] = True
+                stack.append(key)
+    return all(visited)
