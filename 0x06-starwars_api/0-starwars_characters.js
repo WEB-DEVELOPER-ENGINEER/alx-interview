407dcdc83b27e2e548a0f91bd36a94cabca12d07
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-22 18:19:21 +0000

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,14 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+  const characters = JSON.parse(body).characters;
+  exactOrder(characters, 0);
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
+const exactOrder = (characters, x) => {
+  if (x === characters.length) return;
+  request(characters[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+    exactOrder(characters, x + 1);
   });
 };
